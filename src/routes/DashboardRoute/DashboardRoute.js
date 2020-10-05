92bf099bff1a45ba497ac9604c97b43d9bbe30af
Jim Smith <jsmith774@gmail.com>
2020-10-05 15:42:26 -0400

    WIP implmeneting day 2 styling

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,104 +1,51 @@
 import React, { Component } from 'react';
+import SpacedRepetitionService from '../../services/spaced-repetition-api-service';
 
 class DashboardRoute extends Component {
+  state = {
+    language: '',
+    words: [],
+  };
+
+  componentDidMount() {
+    SpacedRepetitionService.getLanguageAndWords().then((res) => {
+      this.setState({
+        language: res.language,
+        words: res.words,
+      });
+    });
+  }
+
   renderWords() {
-    const words = [
-      {
-        id: 1,
-        original: 'original 1',
-        translation: 'translation 1',
-        memory_value: 0,
-        incorrect_count: 0,
-        correct_count: 0,
-        language_id: 1,
-        next: 2,
-      },
-      {
-        id: 2,
-        original: 'original 2',
-        translation: 'translation 2',
-        memory_value: 0,
-        incorrect_count: 0,
-        correct_count: 2,
-        language_id: 1,
-        next: 3,
-      },
-      {
-        id: 3,
-        original: 'original 3',
-        translation: 'translation 3',
-        memory_value: 0,
-        incorrect_count: 3,
-        correct_count: 6,
-        language_id: 1,
-        next: 4,
-      },
-      {
-        id: 4,
-        original: 'original 4',
-        translation: 'translation 4',
-        memory_value: 0,
-        incorrect_count: 2,
-        correct_count: 6,
-        language_id: 1,
-        next: 5,
-      },
-      {
-        id: 5,
-        original: 'original 5',
-        translation: 'translation 5',
-        memory_value: 0,
-        incorrect_count: 0,
-        correct_count: 9,
-        language_id: 1,
-        next: null,
-      },
-    ];
-    return words.map((word) => {
+    const words = this.state.words;
+
+    return words.map((word, i) => {
       return (
-        <li className="word-list-item">
-          <h4>{word.original}</h4>
-          correct answer count: {word.correct_count}
-          <br />
-          incorrect answer count: {word.incorrect_count}
+        <li key={i} className="dashboard-word-list-item">
+          <h4 className="dashboard-word">{word.original}</h4>
+          <div className="dashboard-word-counts">
+            correct answer count: <span>{word.correct_count}</span>
+            <br />
+            incorrect answer count: <span>{word.incorrect_count}</span>
+          </div>
         </li>
       );
     });
   }
 
   render() {
-    const language = { name: 'Test language 1', total_score: '7' }; //TODO api call
-    const { name, total_score } = language;
+    const { name, total_score } = this.state.language;
 
     return (
       <section>
-        <h2>{name}</h2>
+        <h2 className="dashboard-language-name">{name}</h2>
         <div>Total correct answers: {total_score}</div>
         <a href="/learn">Start practicing</a>
-        <h3>Words to practice</h3>
-        <ul className="word-list">{this.renderWords()}</ul>
+        <h3 className="dashboard-subtitle">Words to practice</h3>
+        <ul className="dashboard-word-list">{this.renderWords()}</ul>
       </section>
     );
   }
-  /*
-words.forEach((word, idx) => {
-        cy.get('main section li').eq(idx).within($li => {
-
-          cy.get('h4').should('have.text', word.original)
-
-          cy.root()
-            .should(
-              'contain',
-              `correct answer count: ${word.correct_count}`
-            )
-
-          cy.root()
-            .should(
-              'contain',
-              `incorrect answer count: ${word.incorrect_count}`
-            )
-        })
-*/
 }
 
 export default DashboardRoute;
